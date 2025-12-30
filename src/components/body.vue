ab55346ec74899d3878af200218460acd4701e9e
zhy <zhy@123.com>
2025-12-30 11:56:46 +0800

    0.0.1

---
+++ src/components/body.vue
@@ -0,0 +1,131 @@
+<template>
+    <body class="body-area">
+        <div class="left-area">
+            <ul>
+                <li class='li-text' v-for="i in category_list">
+                    <i class="iconfont icon-mifan"></i>&nbsp;&nbsp;&nbsp;&nbsp;{{i}}</li>
+            </ul>
+            <p class="red">全部菜谱分类</p>
+        </div>
+
+        <div class="mid-area">
+            <div class="mid-up">
+                <!-- 我是中上区域 -->
+                <img src="/static/1.png" alt="">
+                <p class='pre-fonts'>新秀菜谱</p>
+                <p class='pre-fonts'>最新流行</p>
+
+            </div>
+            
+
+        </div>
+
+        <div class="right-area">
+            <div class="login">
+                <p>QQ登录</p>
+                <p>微博登录</p>
+            </div>
+            <!-- <p>我是右边区域</p> -->
+             <div class="rank">
+                <p class="pre-fonts">流行搜索</p>
+                <ol class="rank-1">
+                    <li>1&nbsp;&nbsp;&nbsp;&nbsp;家常菜</li>
+                    <li>2&nbsp;&nbsp;&nbsp;&nbsp;红烧肉</li>
+                    <li>3&nbsp;&nbsp;&nbsp;&nbsp;可乐鸡翅</li>
+                    <li>4&nbsp;&nbsp;&nbsp;&nbsp;排骨</li>
+                </ol>
+             </div>
+             <div>
+                <p class="pre-fonts">流行菜单</p>
+             </div>
+
+        </div>
+        
+    </body>
+</template>
+<script setup>
+import { ref } from 'vue';
+
+const category_list = ref([
+    '家常菜', '快手菜', '下饭菜', '早餐', '肉', '鱼', '蔬菜', '...'
+])
+</script>
+
+
+<style scoped> 
+.body-area{
+    display: flex;
+    justify-content: center;
+    align-items: flex-start; 
+    gap: 40px;
+}
+.left-area{
+    width: 200px;
+    background-color: antiquewhite;
+}
+.right-area{
+    width: 380px;
+    /* background-color: rgb(237, 139, 10); */
+    display: flex;
+    gap: 10px;
+    flex-direction: column;
+    
+}
+.mid-area{
+    width: 650px;
+    background-color: aqua;
+}
+.li-text{
+    line-height: 40px;
+}
+.red{
+    color: brown;
+}
+img {
+  max-width: 100%;
+  max-height: 100%;
+  width: auto;
+  height: auto;
+  object-fit: contain;
+  display: block;
+  margin: auto;
+}
+.mid-up{
+    height: 320px;
+}
+.login{
+    height: 320px;
+    width: 380px;
+      display: flex;
+        flex-direction: column;
+  justify-content: center; /* 水平居中 */
+  align-items: center;     /* 垂直居中 */
+  gap: 10px;
+  background-color: #f7f7f7;
+}
+.rank{    
+    width: 380px;
+    background-color: #fff;
+}
+.pre-fonts{
+    font-size: large;
+    color: rgb(195, 118, 24);
+    font-weight: bold;
+}
+.rank-1{
+    display: flex;
+    gap: 10px;
+    flex-direction: column;
+}
+ul,ol{
+    list-style: none;
+    padding: 0;
+    margin: 0;
+}
+.iconfont {
+  color: rgb(28, 10, 233);      /* 图标颜色 */
+  font-size: 32px; /* 图标大小 */
+}
+
+
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,56 @@
+<template>
+    <headers class="headerc">
+        <span class="logo">下厨房</span>
+        <div class="search-warpper">
+            <input class="search" type="text" placeholder="搜索菜谱, 食材">
+            <span class="search-text">搜索</span>
+        </div>
+        
+        <span>首页</span>
+        <span>菜谱分类</span>
+        <span>菜单</span>
+        <span>注册</span>
+        <span>登录</span>
+    </headers>
+    
+</template>
+
+<style scoped>
+.headerc {
+    display: flex; 
+  text-align:center;
+  align-items: center;  
+  justify-content: center;
+  height: 65px;
+  background: #fff;
+  gap: 80px;
+  font-size: large;
+   border-bottom: 1px solid rgba(0,0,0,0.06); /* 很浅的线 */
+}
+.logo{
+    color: lab(85.75% -7.4 85.15 / 0.996);
+    font-size: xx-large;
+}
+.search{
+    height: 40px;
+    width: 200px;
+    /* border: 1px solid rgba(0, 0, 0, 0.06); */
+      border: none;
+   box-shadow: inset 0 0 0 1px rgba(0, 0, 0, 0.06);
+    font-size: large;
+     outline: none; 
+}
+.search-warpper{
+    height: 40px;
+    display: flex;
+    align-items: center;
+    gap: 0px; /* 搜索框和文字间距，可调 */
+    /* width: 240px; */
+    background-color: #eba50ed6;
+    
+}
+.search-text{
+    width: 60px;
+}
+</style>
+

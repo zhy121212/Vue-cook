34a4fb7caae19dbd15baf19a115a88b33f03d383
10612 <1061260473@qq.com>
2026-01-04 19:26:31 +0800

    0.0.2

--- src/components/index-body.vue
+++ src/components/index-body.vue
@@ -17,10 +17,42 @@
                     <img src="/static/3.png" alt="">
                 </div>
             </div>
+            <div class="body-mid-mid">
+                <div class="subtitle-layout">
+                    <a class="subtitle" href="">新秀菜谱</a>
+                    <a href="">全部</a>
+                </div>
+                <div class="body-mid-mid-detail-img-and-describe">
+                    <div class="body-mid-mid-detail">
+                        <img src="/static/1/1.png" alt="">
+                        <a href="">荔浦芋头褒</a>
+                    </div>
+                    <div class="body-mid-mid-detail">
+                        <img src="/static/1/2.png" alt="">
+                        <a href="">玉米面饼</a>
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="body-mid-down">
+                <div class="subtitle-layout">
+                    <a class="subtitle" href="">最近流行</a>
+                    <a href="">全部</a>
+            </div>
+
+            </div>
 
             
         </div>
-        <div>body-right</div>
+        <div class="body-right">
+            <div class='body-right-top'>
+                <div class='body-right-top-login'>
+                    <a href="">QQ登录</a>
+                    <a href="">微博登录</a>
+                </div>
+            </div>
+        </div>
     </div>
 
     <div></div>
@@ -33,7 +65,7 @@
         display: flex;
         justify-content: center;
         /* align-items: center; */
-        gap: 100px;
+        gap: 60px;
     }
     
     .body-left ul{
@@ -48,6 +80,9 @@
     }
     .body-mid{
         width: 730px;
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
     }
     .body-mid-top{
         height: 400px;
@@ -70,6 +105,51 @@
         flex-shrink: 0;
     }
     
+    .body-mid-mid-detail{
+        width: 200px;
+        /* height: 100px; */
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+    }
+    .body-mid-mid-detail img{
+        object-fit: cover;
+        height: 100%;
+    }
+    .body-mid-mid{
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+    }
+    .body-mid-mid-detail-img-and-describe{
+        display: flex;
+        gap: 10px;
+    }
+    .body-mid-mid-detail a{
+        text-align: center;
+    }
+    .subtitle-layout{
+        display: flex;
+        gap: 15px;
+    }
+    .body-right{
+        width: 300px;
+        
+    }
+    .body-right-top{
+        height: 300px;
+        background-color: whitesmoke;
+
+    }
+    .body-right-top-login{
+        height: 100%;
+        /* background-color: blue; */
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 20px;
+    }
 </style>
 
 <script setup>
